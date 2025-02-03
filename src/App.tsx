b89650100a417cc476fb69823e379859ace0555e
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-02-03 19:34:12 +0600

    add super basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Authorization from './components/Authorization.tsx'
+import Home from './components/Home.tsx'
+import UserProfile from './components/UserProfile.tsx'
+import NotFound from './components/NotFound.tsx'
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+       <Router>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/user">User</Link>
+                        </li>
+                        <li>
+                            <Link to="/authorization">Authorize???</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Routes>
+                    <Route path="/authorization" element={<Authorization />}/>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/user" element={<UserProfile />}/>
+                    <Route path="*" element={<NotFound />}/>
+                </Routes>
+        </Router>
   )
 }
 
