9b6408881c3622ef06193bebeda05bdff51f28fe
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-04 01:46:49 +0600

    small changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <>
             <h1>Home page</h1>
-            <h2>timurkaaa___</h2>c
+            <h2>timurkaaa___</h2>
         </>
     )
 }
