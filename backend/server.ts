6d8590db4a233919407a83f574cb9a7da33e5d19
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-04 16:53:26 +0600

    Added methods for reading and writing users to json

--- backend/server.ts
+++ backend/server.ts
@@ -12,3 +12,19 @@ const FILE_PATH = path.join(__dirname, "../data/users.json");
 
 app.use(express.json());
 app.use(cors());
+
+const readUsers = async (): Promise<User[]> => {
+    try{
+        await fs.access(FILE_PATH);
+        const data = await fs.readFile(FILE_PATH, "utf8");
+        return JSON.parse(data);
+    } catch (error) {
+        await fs.writeFile(FILE_PATH, "[]");
+        return [];
+    }
+}
+
+const writeUser = async (users: User[]): Promise<void> => {
+    await fs.writeFile(FILE_PATH, JSON.stringify(users, null, 2), "utf8");
+}
+
