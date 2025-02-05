2515428c244645d7769adbe4a98ddb82c3c66093
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-02-05 15:29:27 +0600

    add local json fetching

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,15 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
-    Link
 } from "react-router-dom";
 
 function App() {
   return (
        <Router>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/user">User</Link>
-                        </li>
-                        <li>
-                            <Link to="/authorization">Authorize???</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
                 <Routes>
                     <Route path="/authorization" element={<Authorization />}/>
                     <Route path="/" element={<Home />}/>
-                    <Route path="/user" element={<UserProfile />}/>
+                    <Route path="/user/:userId" element={<UserProfile />}/>
                     <Route path="*" element={<NotFound />}/>
                 </Routes>
         </Router>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,6 @@ export default function Home() {
     return (
         <>
             <h1>Home page</h1>
-            <h2>timurkaaa___</h2>c
         </>
     )
 }
