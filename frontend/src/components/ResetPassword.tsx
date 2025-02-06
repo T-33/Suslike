0423c0da205ffa5d099bedd6cdeef8af793c2796
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-07 02:30:56 +0600

    feat: server request processed

--- frontend/src/components/ResetPassword.tsx
+++ frontend/src/components/ResetPassword.tsx
@@ -1,4 +1,23 @@
+import React, {useState} from "react";
+
 export default function ResetPassword(){
+    const [username, setUsername] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleResetPassword = async (event : React.FormEvent) => {
+        event.preventDefault();
+
+        const response = await fetch("http://localhost:3001/reset-password", {
+            method: "POST",
+            headers: {"Content-Type": "application/json",},
+            body: JSON.stringify({})
+        })
+
+        const data = await response.json();
+        setMessage(data.message)
+    }
+
     return(
         <h1>
             You forgot your password.
