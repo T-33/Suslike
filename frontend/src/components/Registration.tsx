43d0bae17c7428be15c4cb32379a7411f6bdfd40
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-06 10:30:16 +0600

    feat: added handlers for changing fields and form submission

--- frontend/src/components/Registration.tsx
+++ frontend/src/components/Registration.tsx
@@ -5,4 +5,42 @@ export default function Registration(){
     const [formData, setFormData] = useState<Partial<User>>({});
     const [error, setError] = useState<string | null>(null);
     const [success, setSuccess] = useState<string | null>(null);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const [name, value] = event.target;
+        setFormData({
+            ...formData,
+            [name]: value});
+    }
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        setError(null);
+        setSuccess(null);
+
+        try{
+            const response = await fetch('http://localhost:3001/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({formData})
+            })
+
+            if(!response.ok){
+                const data = await response.json();
+                throw new Error(data.error || 'Something went wrong');
+            }
+
+            const data = await response.json();
+            setSuccess("User successfully registered");
+            console.log("Successfully registered" + data);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    return (
+
+    )
 }
