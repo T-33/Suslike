3eac9d24b8aba6549c5b98dfb56b43aa2d216aa0
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-06 10:22:31 +0600

    add: necessary imports and states were added to store the form data

--- frontend/src/components/Registration.tsx
+++ frontend/src/components/Registration.tsx
@@ -1,3 +1,8 @@
+import React, {useState} from 'react';
+import User from '../types/User.ts'
+
 export default function Registration(){
-    return
+    const [formData, setFormData] = useState<Partial<User>>({});
+    const [error, setError] = useState<string | null>(null);
+    const [success, setSuccess] = useState<string | null>(null);
 }
