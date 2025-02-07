81640c7f3050cd5b5c3fb868d35923d84e4fbac2
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-07 15:57:21 +0600

    style:
    the page has been restyled

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,5 +1,35 @@
+import {Link} from "react-router-dom";
+import {useState, useEffect} from "react";
+import gopherDead from "../images/gopher-dead.png";
+import gopherConfused from "../images/gopher-confused.png";
+import gopherCrying from "../images/gopher-crying-river.png";
+import '../styles/index.css'
+
+const images = [gopherDead, gopherConfused, gopherCrying]
+
 export default function NotFound() {
+    const [randomImage, setRandomImage] = useState("");
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        setRandomImage(images[randomIndex]);
+    }, [])
+
+
     return (
-        <h1>Page not found</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-6xl font-bold text-white-800">404</h1>
+            <p className="text-xl text-gray-600 mt-4">Page not found</p>
+            {randomImage && (
+                <img
+                    src={randomImage}
+                    alt="Not Found"
+                    className="my-6 w-40 md:w-80 scale-100"
+                    style={{imageRendering: "pixelated"}}
+                />
+            )}
+            <Link to="/" className="border p-2 rounded-xl">
+                Go Home
+            </Link>
+        </div>
     )
 }
