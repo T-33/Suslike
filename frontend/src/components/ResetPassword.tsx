43b7273ec3c79ae5ecaeee1219457ee116921cbc
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-13 01:20:42 +0600

    style: design for the ResetPassword.tsx was added

--- frontend/src/components/ResetPassword.tsx
+++ frontend/src/components/ResetPassword.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 export default function ResetPassword() {
     const [username, setUsername] = useState("");
@@ -31,46 +32,58 @@ export default function ResetPassword() {
     }
 
     return (
-        <div className="auth-block">
-            <h1 className="text-3xl my-5">Reset Password</h1>
-            <form onSubmit={handleResetPassword}>
-                <div>
-                    <label htmlFor="username">Username: </label>
-                    <input
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                        className="border"
-                    />
-                </div>
-                <div>
-                    <label htmlFor="newPassword">New Password: </label>
-                    <input
-                        type="password"
-                        id="newPassword"
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                        required
-                        className="border"
-                    />
-                </div>
+        <div className="flex flex-col justify-center items-center gap-5">
+            <div className="border border-gray-200 p-9 w-full max-w-sm">
+                <form onSubmit={handleResetPassword} className="flex flex-col gap-2">
+                    <h1 className="text-3xl mb-5 self-center">Reset Password</h1>
+                    <div>
+                        <input
+                            type="text"
+                            id="username"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                            className="w-full p-1.5 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"/>
 
-                <div>
-                    <label htmlFor="confirmPassword">Confirm Password: </label>
-                    <input
-                        type="password"
-                        id="confirmPassword"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        required
-                        className="border"
-                    />
-                </div>
-                <button type="submit" className="my-3">Reset Password</button>
-            </form>
-            {message && <p>{message}</p>}
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            id="newPassword"
+                            placeholder="New password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            required
+                            className="w-full p-1.5 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+
+                    </div>
+
+                    <div>
+                        <input
+                            type="password"
+                            id="confirmPassword"
+                            placeholder="Confirm password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            required
+                            className="w-full p-1.5 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+
+                    </div>
+                    <button type='submit'
+                            className="mt-4 bg-amber-50 text-gray-700 p-2 border rounded-2xl cursor-pointer hover:text-black">Reset
+                        password
+                    </button>
+                </form>
+                {message && <p>{message}</p>}
+            </div>
+
+            <p>OR</p>
+
+            <div className="flex flex-col items-center p-3.5 border border-gray-200 w-full max-w-sm">
+                <p>No account?</p>
+                <Link to="/register" className="text-blue-400 hover:text-blue-500">Create new account</Link>
+            </div>
         </div>
     );
 }
